510bf0d840a18014e7b80a33ecaf3e8a1f9bb506
Sl1mj1m14 <keiler104m@gmail.com>
2024-11-06 22:25:18 -0500

    Changed direct references to self references

--- converter/src/random_level_worker.rs
+++ converter/src/random_level_worker.rs
@@ -17,7 +17,7 @@ impl Distort {
         }
     }
 
-    fn get_value (self, x: f64, y: f64) -> f64 {
+    fn get_value (&self, x: f64, y: f64) -> f64 {
         return self.source.get_value(x + self.distort.get_value(x, y), y);
     }
 }
@@ -48,7 +48,7 @@ impl PerlinNoise {
 
     }
 
-    fn get_value (self, x: f64, y: f64) -> f64 {
+    fn get_value (&self, x: f64, y: f64) -> f64 {
 
         let mut value: f64 = 0.0;
         let mut pow: f64 = 1.0;
@@ -67,12 +67,12 @@ impl PerlinNoise {
 *************************/
 
 #[derive(Clone)]
-struct ImprovedNoise {
+pub struct ImprovedNoise {
     p: Vec<i32>
 }
 
 impl ImprovedNoise {
-    fn new (random: f64) -> Self {
+    pub fn new (random: f64) -> Self {
         let mut p: Vec<i32> = Vec::new();
 
         for i in 0..256 {
@@ -95,22 +95,22 @@ impl ImprovedNoise {
         }
     }
 
-    fn fade_curve (d0: f64) -> f64 {
+    pub fn fade_curve (&self, d0: f64) -> f64 {
         return d0 * d0 * d0 * (d0 * (d0 * 6.0 - 15.0) + 10.0);
     }
 
-    fn lerp (d0: f64, d1: f64, d2: f64) -> f64 {
+    pub fn lerp (&self, d0: f64, d1: f64, d2: f64) -> f64 {
         return d1 + d0 * (d2 - d1);
     }
 
-    fn grad (i: i32, d0: f64, d1: f64, d2: f64) -> f64 {
+    pub fn grad (&self, i: i32, d0: f64, d1: f64, d2: f64) -> f64 {
         let d3: f64 = if (i & 15) < 8 {d0} else {d1};
         let d4: f64 = if i < 4 {d1} else {if i != 12 && i != 14 {d2} else {d0}};
 
         return (if (i & 1) == 0 {d3} else {-d3}) + (if (i & 2) == 0 {d4} else {-d4});
     }
 
-    fn get_value (self, d0: f64, d1: f64) -> f64 {
+    pub fn get_value (&self, d0: f64, d1: f64) -> f64 {
 
         let mut d2: f64 = 0.0;
         let mut d3: f64 = d1;
@@ -124,9 +124,9 @@ impl ImprovedNoise {
         d4 -= d4.floor();
         d3 -= d3.floor();
         d2 = 0.0 - F0.floor();
-        let d5: f64 = ImprovedNoise::fade_curve(d4);
-        let d6: f64 = ImprovedNoise::fade_curve(d3);
-        let d7: f64 = ImprovedNoise::fade_curve(d2);
+        let d5: f64 = self.fade_curve(d4);
+        let d6: f64 = self.fade_curve(d3);
+        let d7: f64 = self.fade_curve(d2);
         let mut l: i32 = self.p[i as usize] + j;
         let i1: i32 = self.p[l as usize] + k;
 
@@ -135,6 +135,6 @@ impl ImprovedNoise {
         j = self.p[i as usize] + k;
         i = self.p[(i + 1) as usize] + k;
 
-        return ImprovedNoise::lerp(d7, ImprovedNoise::lerp(d6, ImprovedNoise::lerp(d5, ImprovedNoise::grad(self.p[i1 as usize], d4, d3, d2), ImprovedNoise::grad(self.p[j as usize], d4 - 1.0, d3, d2)), ImprovedNoise::lerp(d5, ImprovedNoise::grad(self.p[l as usize], d4, d3 - 1.0, d2), ImprovedNoise::grad(self.p[i as usize], d4 - 1.0, d3 - 1.0, d2))), ImprovedNoise::lerp(d6, ImprovedNoise::lerp(d5, ImprovedNoise::grad(self.p[(i1 + 1) as usize], d4, d3, d2 - 1.0), ImprovedNoise::grad(self.p[(j + 1) as usize], d4 - 1.0, d3, d2 - 1.0)), ImprovedNoise::lerp(d5, ImprovedNoise::grad(self.p[(l + 1) as usize], d4, d3 - 1.0, d2 - 1.0), ImprovedNoise::grad(self.p[(i + 1) as usize], d4 - 1.0, d3 - 1.0, d2 - 1.0))));
+        return self.lerp(d7, self.lerp(d6, self.lerp(d5, self.grad(self.p[i1 as usize], d4, d3, d2), self.grad(self.p[j as usize], d4 - 1.0, d3, d2)), self.lerp(d5, self.grad(self.p[l as usize], d4, d3 - 1.0, d2), self.grad(self.p[i as usize], d4 - 1.0, d3 - 1.0, d2))), self.lerp(d6, self.lerp(d5, self.grad(self.p[(i1 + 1) as usize], d4, d3, d2 - 1.0), self.grad(self.p[(j + 1) as usize], d4 - 1.0, d3, d2 - 1.0)), self.lerp(d5, self.grad(self.p[(l + 1) as usize], d4, d3 - 1.0, d2 - 1.0), self.grad(self.p[(i + 1) as usize], d4 - 1.0, d3 - 1.0, d2 - 1.0))));
     }
 }
